f2ccffa8ec736b737ef8d13e776feee5740498f8
Pim Snel <pim@lingewoud.nl>
2012-06-28 00:00:15 +0200

    stop unneeded build files

--- Site Lighter/build/Site Lighter.build/Debug/Site Lighter.build/Objects-normal/x86_64/Preferences.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/pim/github/local/SiteLighter/Site\ Lighter/Site\ Lighter/Preferences.m \
-  /Users/pim/github/local/SiteLighter/Site\ Lighter/Site\ Lighter/Preferences.h
--- Site Lighter/build/Site Lighter.build/Debug/Site Lighter.build/Objects-normal/x86_64/WebViewScreenShot.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/pim/github/local/SiteLighter/Site\ Lighter/Site\ Lighter/WebViewScreenShot.m \
-  /Users/pim/github/local/SiteLighter/Site\ Lighter/Site\ Lighter/WebViewScreenShot.h
